a03a017d212a7e992ef1ba4f16cb86db55b35321
David Schuetz <38507773+DaveSchuetz@users.noreply.github.com>
2018-08-21 20:42:57 -0400

    Hide delete

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ server.use(session({secret: 'schuetz-project2-cookbook'}))
 
 server.set('view engine', 'hbs')
 server.use(express.static(__dirname ))
-
+hbs.registerHelper('ifCond', function(v1, v2, options) {
+  if(v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+})
 
 require('./config/passport')(passport)
 server.use(passport.initialize())
